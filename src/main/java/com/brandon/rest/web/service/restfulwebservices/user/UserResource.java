95aac921b54f7b45203eb29e6d42cecfbefc0fb4
Brandon Lewis <bplewis98@gmail.com>
2022-04-29 13:59:13 -0500

    Error handling

--- src/main/java/com/brandon/rest/web/service/restfulwebservices/user/UserResource.java
+++ src/main/java/com/brandon/rest/web/service/restfulwebservices/user/UserResource.java
@@ -1,9 +1,13 @@
 package com.brandon.rest.web.service.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.Servlet;
+import java.net.URI;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
@@ -20,7 +24,31 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(int id){
-        return service.findOne(id);
+    public User retrieveUser(@PathVariable int id){
+        User user = service.findOne(id);
+        if(user == null){
+            throw new UserNotFoundException("id-" + id);
+        }
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity createUser(@RequestBody User user){
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        User user = service.deleteById(id);
+        if(user == null) {
+            throw new UserNotFoundException("id- " + id);
+        }
     }
 }
